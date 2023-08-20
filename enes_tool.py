836e9590a98c7595eba5abc70a6fdf36c9e773bb
Diego <diegonzalez_96@hotmail.com>
2023-08-20 17:14:43 +0200

    Code Update
    Code Update

--- enes_tool.py
+++ enes_tool.py
@@ -105,9 +105,88 @@ def detect_os(ip):
     except:
         print(f"{Fore.CYAN}{ip}{Style.RESET_ALL} - No se pudo detectar el Sistema Operativo")
 
+def service_fingerprinting(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)
+        sock.connect((ip, port))
+        banner = sock.recv(1024).decode().strip()
+        print(f"Fingerprinting en {Fore.CYAN}{ip}{Style.RESET_ALL}:{Fore.CYAN}{port}{Style.RESET_ALL} - {Fore.GREEN}{banner}{Style.RESET_ALL}")
+        sock.close()
+    except:
+        print(f"No se pudo realizar el servicio de fingerprinting en el puerto {Fore.CYAN}{port}{Style.RESET_ALL}", Fore.YELLOW + "(Error)" + Style.RESET_ALL)
+
+def user_enumeration(ip, port):
+    try:
+        url = f"http://{ip}:{port}/"
+        response = requests.get(url, timeout=2)
+        if response.status_code == 200:
+            users = re.findall(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})', response.text)
+            if users:
+                users = list(set(users))
+                print(f"Usuarios enumerados en {Fore.CYAN}{ip}{Style.RESET_ALL}:{Fore.CYAN}{port}{Style.RESET_ALL}: {Fore.GREEN}{users}{Style.RESET_ALL}")
+            else:
+                print(f"No se encontraron usuarios en {Fore.CYAN}{ip}{Style.RESET_ALL}:{Fore.CYAN}{port}{Style.RESET_ALL}")
+        else:
+            print(f"La solicitud HTTP falló en {Fore.CYAN}{ip}{Style.RESET_ALL}:{Fore.CYAN}{port}{Style.RESET_ALL} - Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"No se pudo realizar la enumeración de usuarios en el puerto {Fore.CYAN}{port}{Style.RESET_ALL}", Fore.YELLOW + f"(Error: {e})" + Style.RESET_ALL)
+
+def run_all_options(scanned_ips, ports_to_scan):
+    ips = arp_ping(network)
+    scanned_ips.extend(ips)
+    scanned_ips = list(set(scanned_ips))
+
+    for ip in ips:
+        print(Fore.MAGENTA + f"\n==== Ping para {ip} ====" + Style.RESET_ALL)
+        tcp_ping(ip, 80)
+        udp_ping(ip, 53)
+        icmp_ping(ip)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + f"\n==== Enumerando puertos abiertos en {ip} ====" + Style.RESET_ALL)
+        open_ports = tcp_connect_scan(ip, ports_to_scan)
+        print(f"Open ports on {Fore.CYAN}{ip}{Style.RESET_ALL}: {Fore.GREEN}{open_ports}{Style.RESET_ALL}")
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Descubriendo si algún elemento está filtrando puertos ====" + Style.RESET_ALL)
+        print(Fore.MAGENTA + f"\n==== Realizando ACK Scan en {ip} ====" + Style.RESET_ALL)
+        for port in ports_to_scan:
+            ack_scan(ip, port)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Realizando Técnicas de banner grabbing ====" + Style.RESET_ALL)
+        print(Fore.MAGENTA + f"\n==== Realizando Banner Grabbing en {ip} ====" + Style.RESET_ALL)
+        for port in ports_to_scan:
+            banner_grabbing(ip, port)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Realizando Evaluación de cabeceras HTTP ====" + Style.RESET_ALL)
+        print(Fore.MAGENTA + f"\n==== Realizando HTTP Header Evaluation en {ip} ====" + Style.RESET_ALL)
+        for port in ports_to_scan:
+            http_header_evaluation(ip, port)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Detectando Sistema Operativo ====" + Style.RESET_ALL)
+        detect_os(ip)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Realizando Fingerprinting de Servicios ====" + Style.RESET_ALL)
+        print(Fore.MAGENTA + f"\n==== Realizando Service Fingerprinting en {ip} ====" + Style.RESET_ALL)
+        for port in ports_to_scan:
+            service_fingerprinting(ip, port)
+
+    for ip in scanned_ips:
+        print(Fore.MAGENTA + "\n==== Realizando Enumeración de Usuarios ====" + Style.RESET_ALL)
+        print(Fore.MAGENTA + f"\n==== Realizando User Enumeration en {ip} ====" + Style.RESET_ALL)
+        for port in ports_to_scan:
+            user_enumeration(ip, port)
+
+
+
 if __name__ == "__main__":
-    network = "192.168.1.0/24"
-    ports_to_scan = [20, 21, 22, 80, 443] #Añadir mas puertos
+    network = input("Introduce la red local(e.g., 192.168.1.0/24): ")
+    ports_to_scan = [21, 22, 25, 53, 67, 68, 69, 80, 110, 123, 137, 138, 139, 143, 161, 162, 179, 389, 427, 443, 465, 514, 520, 587, 636, 993, 995, 1194, 1701, 1723, 3306, 3389, 8080]  # Puertos mas comunes
     scanned_ips = []
 
     while True:
@@ -118,8 +197,12 @@ if __name__ == "__main__":
         print("4. Técnicas de banner grabbing (Banner Grabbing)")
         print("5. Evaluación de cabeceras HTTP para obtener versiones de software (HTTP Header Evaluation)")
         print("6. Detección del sistema operativo")
+        print("7. Fingerprinting de Servicios (Service Fingerprinting)")
+        print("8. Enumeración de Usuarios (User Enumeration)")
+        print("9. Todas las opciones anteriores a la vez")
+        # print("10.Enumeracion de varias herramientas previas")
         print("0. Salir")
-        choice = input("Elije una opción 0-6: ")
+        choice = input("Elije una opción (0-9): ")
 
         if choice == "1":
             ips = arp_ping(network)
@@ -174,8 +257,45 @@ if __name__ == "__main__":
                 print(Fore.MAGENTA + "\n==== Detectando Sistema Operativo ====" + Style.RESET_ALL)
                 for ip in scanned_ips:
                     detect_os(ip)
+        elif choice == "7":
+            if not scanned_ips:
+                print("Debes ejecutar la opción 1 primero para obtener las IPs.")
+            else:
+                print(Fore.MAGENTA + "\n==== Realizando Fingerprinting de Servicios ====" + Style.RESET_ALL)
+                for ip in scanned_ips:
+                    print(Fore.MAGENTA + f"\n==== Realizando Service Fingerprinting en {ip} ====" + Style.RESET_ALL)
+                    for port in ports_to_scan:
+                        service_fingerprinting(ip, port)
+        elif choice == "8":
+            if not scanned_ips:
+                print("Debes ejecutar la opción 1 primero para obtener las IPs.")
+            else:
+                print(Fore.MAGENTA + "\n==== Realizando Enumeración de Usuarios ====" + Style.RESET_ALL)
+                for ip in scanned_ips:
+                    print(Fore.MAGENTA + f"\n==== Realizando User Enumeration en {ip} ====" + Style.RESET_ALL)
+                    for port in ports_to_scan:
+                        user_enumeration(ip, port)
+        elif choice == "9":
+            if not scanned_ips:
+                print("Debes ejecutar la opción 1 primero para obtener las IPs.")
+            else:
+                run_all_options(scanned_ips, ports_to_scan)
+        # elif choice == "10":
+        #     for ip in scanned_ips:
+        #         open_ports = run_nmap(ip)
+    
+        #     if open_ports:
+        #             print("Open ports:", open_ports)
+        #             for port in open_ports:
+        #                 run_nikto(target_ip + ":" + port)
+        #             else:
+        #                 print("No open ports found.")
+
+        #             print("All scans and enumerations completed.")
+
         elif choice == "0":
             print(Fore.MAGENTA + "Saliendo..." + Style.RESET_ALL)
             break
         else:
-            print(Fore.RED + "Opción inválida. Por favor, elije una opción válida (1/2/3/4/5/6/0)." + Style.RESET_ALL)
+            print(Fore.RED + "Opción inválida. Por favor, elije una opción válida (0-9)." + Style.RESET_ALL)
+            
